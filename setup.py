c7c3a80014c4a568ad1d045c2fb1e3622f1894a0
Justin <justinthong93@gmail.com>
2021-07-23 01:50:15 +0800

    change main folder to jt. not cliffdemo

--- setup.py
+++ setup.py
@@ -1,71 +1,58 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_packages, setup
 
-PROJECT = 'cliffdemo'
+PROJECT = "jt"
 
 # Change docs/sphinx/conf.py too!
-VERSION = '0.1'
+VERSION = "0.1"
 
 try:
-    long_description = open('README.rst', 'rt').read()
+    long_description = open("README.rst", "rt").read()
 except IOError:
-    long_description = ''
+    long_description = ""
 
 setup(
     name=PROJECT,
     version=VERSION,
-
-    description='Demo app for cliff',
+    description="Demo app for cliff",
     long_description=long_description,
-
-    author='Doug Hellmann',
-    author_email='doug.hellmann@gmail.com',
-
-    url='https://github.com/openstack/cliff',
-    download_url='https://github.com/openstack/cliff/tarball/master',
-
+    author="Doug Hellmann",
+    author_email="doug.hellmann@gmail.com",
+    url="https://github.com/openstack/cliff",
+    download_url="https://github.com/openstack/cliff/tarball/master",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Intended Audience :: Developers',
-        'Environment :: Console',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
     ],
-
-    platforms=['Any'],
-
+    platforms=["Any"],
     scripts=[],
-
     provides=[],
-    install_requires=['cliff'],
-
+    install_requires=["cliff"],
     namespace_packages=[],
     packages=find_packages(),
     include_package_data=True,
-
     entry_points={
-        'console_scripts': [
-            'cliffdemo = cliffdemo.main:main'
+        "console_scripts": ["jt = jt.main:main"],
+        "cliff.demo": [
+            "simple = jt.simple:Simple",
+            "two_part = jt.simple:Simple",
+            "error = jt.simple:Error",
+            "list files = jt.list:Files",
+            "files = jt.list:Files",
+            "file = jt.show:File",
+            "show file = jt.show:File",
+            "unicode = jt.encoding:Encoding",
+            "hooked = jt.hook:Hooked",
         ],
-        'cliff.demo': [
-            'simple = cliffdemo.simple:Simple',
-            'two_part = cliffdemo.simple:Simple',
-            'error = cliffdemo.simple:Error',
-            'list files = cliffdemo.list:Files',
-            'files = cliffdemo.list:Files',
-            'file = cliffdemo.show:File',
-            'show file = cliffdemo.show:File',
-            'unicode = cliffdemo.encoding:Encoding',
-            'hooked = cliffdemo.hook:Hooked',
-        ],
-        'cliff.demo.hooked': [
-            'sample-hook = cliffdemo.hook:Hook',
+        "cliff.demo.hooked": [
+            "sample-hook = jt.hook:Hook",
         ],
     },
-
     zip_safe=False,
 )
